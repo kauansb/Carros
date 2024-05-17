b8cd5ff93c93ffbcaa908ab30c576776e766b080
Kauan Santana Barros <kauansbflu@gmail.com>
2024-05-17 14:34:26 -0300

    fix
    correção de métodos clean no form.py

--- carros/cars/forms.py
+++ carros/cars/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from cars.models import Brand, Car
+from cars.models import Car
 
 
 class CarModelForm(forms.ModelForm):
@@ -8,13 +8,13 @@ class CarModelForm(forms.ModelForm):
         fields = '__all__' # Uso de todos os campos disponíveis em Car
 
     def clean_value(self): # Regra para o campo de value com uso de validação automática do django
-        value = self.cleaned_data.get['value']
+        value = self.cleaned_data.get('value')
         if value < 20000:
             self.add_error('value', 'Valor mínimo do carro deve ser R$20.000')
         return value
 
     def clean_factory_year(self):
-        factory_year = self.cleaned_data.get['factory_year']
+        factory_year = self.cleaned_data.get('factory_year')
         if factory_year < 1980:
-            self.add_error('factoy_year', 'Não é possível cadastrar carros fabricados antes de 1980')
+            self.add_error('factory_year', 'Não é possível cadastrar carros fabricados antes de 1980')
             return factory_year
