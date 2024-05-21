508d88e289f9a6d8e577b684966c5a5d29068860
Kauan Santana Barros <kauansbflu@gmail.com>
2024-05-21 15:14:21 -0300

    Update forms.py

--- carros/cars/forms.py
+++ carros/cars/forms.py
@@ -12,9 +12,3 @@ class CarModelForm(forms.ModelForm):
         if value < 20000:
             self.add_error('value', 'Valor mínimo do carro deve ser R$20.000')
         return value
-
-    def clean_factory_year(self):
-        factory_year = self.cleaned_data.get('factory_year')
-        if factory_year < 1980:
-            self.add_error('factory_year', 'Não é possível cadastrar carros fabricados antes de 1980')
-            return factory_year
