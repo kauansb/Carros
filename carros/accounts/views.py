d483775da698504a33eb6ece12a8ca171147475e
Kauan Santana Barros <kauansbflu@gmail.com>
2024-05-23 11:47:25 -0300

    feat
    add mensagens de feedback no app

--- carros/accounts/views.py
+++ carros/accounts/views.py
@@ -2,36 +2,43 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib import messages
 
 class RegisterView(View):
     def get(self, request):
         user_form = UserCreationForm()
         return render(request, 'register.html', {'user_form': user_form})
     
-    def post(self,request):
+    def post(self, request):
         user_form = UserCreationForm(request.POST)
         if user_form.is_valid():
             user_form.save()
+            messages.success(request, 'Registro realizado com sucesso! Faça login para continuar.')
             return redirect('login')
+        else:
+            messages.error(request, 'Erro ao registrar. Verifique os dados e tente novamente.')
         return render(request, 'register.html', {'user_form': user_form})
 
 class LoginView(View):
     def get(self, request):
-        login_form = AuthenticationForm(request.POST)
-        return render(request,'login.html', {'login_form':login_form})
+        login_form = AuthenticationForm()
+        return render(request, 'login.html', {'login_form': login_form})
     
     def post(self, request):
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password) # Validação para verificar se usuário existe
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, 'Login efetuado com sucesso!')
             return redirect('cars_list')
         else:
+            messages.error(request, 'Usuário ou senha incorretos. Tente novamente.')
             login_form = AuthenticationForm()
-        return render(request,'login.html', {'login_form':login_form})
+        return render(request, 'login.html', {'login_form': login_form})
 
 class LogoutView(View):
     def get(self, request):
         logout(request)
+        messages.success(request, 'Logout efetuado com sucesso!')
         return redirect('login')
--- carros/cars/views.py
+++ carros/cars/views.py
@@ -1,9 +1,10 @@
-from cars.models import Car
-from cars.forms import CarModelForm
+from django.contrib import messages
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required, permission_required
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from cars.models import Car
+from cars.forms import CarModelForm
 
 # Class Based Views
 class CarsListView(ListView):
@@ -30,6 +31,11 @@ class NewCarCreateView(CreateView):
     template_name = 'new_car.html'
     success_url = '/cars/' # redireciona para a url ao cadastrar um carro com sucesso
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Carro cadastrado com sucesso!')
+        return response
+
 @method_decorator(login_required(login_url='login'), name='dispatch')
 @method_decorator(permission_required('cars.change_car', raise_exception=True), name='dispatch')
 class CarUpdateView(UpdateView):
@@ -40,9 +46,19 @@ class CarUpdateView(UpdateView):
     def get_success_url(self): # redireciona para o carro editado
         return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Carro atualizado com sucesso!')
+        return response
+
 @method_decorator(login_required(login_url='login'), name='dispatch')
 @method_decorator(permission_required('cars.delete_car', raise_exception=True), name='dispatch')
 class CarDeleteView(DeleteView):
     model = Car
     template_name = 'car_delete.html'
     success_url = '/cars/'
+
+    def delete(self, request, *args, **kwargs):
+        response = super().delete(request, *args, **kwargs)
+        messages.success(request, 'Carro deletado com sucesso!')
+        return response
